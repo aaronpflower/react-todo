72a78cdf2840647f5fa0c98a1d3002ac8ebe02f5
Aaron Flower <aaronpflower@gmail.com>
2017-02-10 14:53:12 -0700

    initial commit

---
+++ client/index.js
@@ -0,0 +1,5 @@
+const React = require('react')
+const ReactDOM = require('react-dom')
+// const routes = require('./config/routes')
+
+ReactDOM.render(<h1>Hello World</h1>, document.getElementById('app'))
